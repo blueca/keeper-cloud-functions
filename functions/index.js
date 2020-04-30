018845a987e7d0dab7bf75ef9d23fee78d74a745
blueca <nickoldacre@outlook.com>
2020-04-30 16:28:24 +0100

    test function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,30 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.newUser = functions.firestore
+  .document('users/{any}')
+  .onCreate((change, context) => {
+    const userName = change.data().username;
+    const userFcm = change.data().fcm;
+
+    const notificationContent = {
+      notification: {
+        title: `Welcome ${userName}`,
+        body: 'Thanks for signing up',
+        icon: 'default',
+        sound: 'default',
+      },
+    };
+
+    return admin
+      .messaging()
+      .sendToDevice(userFcm, notificationContent)
+      .then((result) => {
+        console.log('Notification sent successfully');
+        return null;
+      })
+      .catch((error) => {
+        console.log('Notification sent failed', error);
+        return null;
+      });
+  });
