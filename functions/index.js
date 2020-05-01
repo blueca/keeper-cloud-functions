6a2cf401e94513ffd629fc6053808dc45f1fa76f
blueca <nickoldacre@outlook.com>
2020-05-01 11:30:48 +0100

    update change/context to get function working

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 exports.outstandingHabits = functions.firestore
   .document('users/{userId}')
   .onUpdate((change, context) => {
     const oldOutstanding = change.before.data().outstanding;
     const newOutstanding = change.after.data().outstanding;
-    const userName = change.data().username;
-    const userFcm = change.data().fcm;
+    const userName = change.after.data().username;
+    const userFcm = change.after.data().fcm;
 
     const notificationContent = {
       notification: {
